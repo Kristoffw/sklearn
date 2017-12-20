c76bd946d613c875f9ed1cd15035c31d7498356e
wangjiuyong <wangjiuyong@tuniu.com>
2017-12-20 11:35:58 +0800

    使用train_test_split代替代码生成交叉验证

--- com/nuaa/sklearn_demo/feature_selection_demo/chi2_demo.py
+++ com/nuaa/sklearn_demo/feature_selection_demo/chi2_demo.py
@@ -4,6 +4,7 @@ from sklearn.feature_selection import chi2
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 # 与sklearn_svm_isas_demo.py对比，这边先进行特征提取，然后在用SVM进行分类算法
@@ -14,21 +15,11 @@ iris_data, iris_target = iris.data, iris.target
 print(iris_data.shape)
 new_data = SelectKBest(chi2, k=3).fit_transform(iris_data, iris_target)
 print(new_data.shape)
-#new_data = StandardScaler().fit_transform(new_data)
+new_data = StandardScaler().fit_transform(new_data)
 
-# 拼接成150*5的矩阵，进行乱序
-iris_concatenate_data = np.concatenate((new_data, np.array(iris_target).reshape(new_data.shape[0], -1)), axis=1)
-np.random.shuffle(iris_concatenate_data)
-
-# 重新获取数据
-iris_data = iris_concatenate_data[:, :-1]
-iris_target = iris_concatenate_data[:, -1]
-
-traing_data = iris_data[0:120]
-traing_target = iris_target[0:120]
-
-test_data = iris_data[120:150]
-test_target = iris_target[120:150]
+traing_data,test_data, traing_target, test_target = train_test_split(new_data,
+                                                                     iris_target,
+                                                   test_size = 0.2)
 
 rbf_svc = SVC()
 rbf_svc.fit(traing_data, traing_target)
--- com/nuaa/sklearn_demo/sklearn/svm/sklearn_svm_isas_demo.py
+++ com/nuaa/sklearn_demo/sklearn/svm/sklearn_svm_isas_demo.py
@@ -4,25 +4,17 @@ from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 iris_orign_datasets = datasets.load_iris()
 # 现将数据标准化
 iris_orign_data = StandardScaler().fit_transform(iris_orign_datasets.data)
 iris_orign_target = iris_orign_datasets.target
 
-# 拼接成150*5的矩阵，进行乱序
-iris_concatenate_data = np.concatenate((iris_orign_data, np.array(iris_orign_target).reshape(iris_orign_data.shape[0], -1)), axis=1)
-np.random.shuffle(iris_concatenate_data)
 
-# 重新获取数据
-iris_data = iris_concatenate_data[:, :-1]
-iris_target = iris_concatenate_data[:, -1]
-
-traing_data = iris_data[0:120]
-traing_target = iris_target[0:120]
-
-test_data = iris_data[120:150]
-test_target = iris_target[120:150]
+traing_data,test_data, traing_target, test_target = train_test_split(iris_orign_data,
+                                                   iris_orign_target,
+                                                   test_size = 0.2)
 
 rbf_svc = SVC()
 rbf_svc.fit(traing_data, traing_target)
