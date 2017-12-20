b83c83efab4a63e1a96d5ef81b96fe6d344d4100
wangjiuyong <wangjiuyong@tuniu.com>
2017-12-20 11:06:38 +0800

    增加乱序机制提高检测性

--- com/nuaa/sklearn_demo/feature_selection_demo/chi2_demo.py
+++ com/nuaa/sklearn_demo/feature_selection_demo/chi2_demo.py
@@ -1,17 +1,23 @@
-from sklearn.svm import SVC, SVR
-
-from sklearn import datasets
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from sklearn import metrics
 import numpy as np
 
-iris_orign_datasets = datasets.load_iris()
-# 现将数据标准化
-iris_orign_data = StandardScaler().fit_transform(iris_orign_datasets.data)
-iris_orign_target = iris_orign_datasets.target
+# 与sklearn_svm_isas_demo.py对比，这边先进行特征提取，然后在用SVM进行分类算法
+
+iris = load_iris()
+iris_data, iris_target = iris.data, iris.target
+
+print(iris_data.shape)
+new_data = SelectKBest(chi2, k=3).fit_transform(iris_data, iris_target)
+print(new_data.shape)
+#new_data = StandardScaler().fit_transform(new_data)
 
 # 拼接成150*5的矩阵，进行乱序
-iris_concatenate_data = np.concatenate((iris_orign_data, np.array(iris_orign_target).reshape(iris_orign_data.shape[0], -1)), axis=1)
+iris_concatenate_data = np.concatenate((new_data, np.array(iris_target).reshape(new_data.shape[0], -1)), axis=1)
 np.random.shuffle(iris_concatenate_data)
 
 # 重新获取数据
@@ -31,5 +37,5 @@ predict = rbf_svc.predict(test_data)
 
 # 获取标签和对应的分类名称
 labels = [0, 1, 2]
-target_names = iris_orign_datasets.target_names
+target_names = iris.target_names
 print(metrics.classification_report(test_target, predict, labels, target_names))
